d7cbbdd59c7431f6cd995f4f144775a200fa3144
Shankara Subramanian <89283498+YEGCSharpDev@users.noreply.github.com>
2026-07-05 12:31:10 -0600

    Add flake.nix for Pandoc static site environment

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Pandoc minimal static site environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [ pkgs.pandoc ];
+          };
+        });
+    };
+}
